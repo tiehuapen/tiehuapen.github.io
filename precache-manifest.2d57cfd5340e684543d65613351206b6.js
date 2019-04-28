bd6e8482bc9fce042d4ac10906a2c2f5c2ccc3c6
jianhuap <jianhuap@qq.com>
2019-04-28 22:53:39 +0800

    Updates

---
+++ precache-manifest.2d57cfd5340e684543d65613351206b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1717bf7021f3d648430fc073524d2e84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3928c4dad8c35dfb98f2",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "68f5801b4b62c6344d8a",
+    "url": "/static/css/main.e82bd986.chunk.css"
+  },
+  {
+    "revision": "3928c4dad8c35dfb98f2",
+    "url": "/static/js/2.fa2df3b4.chunk.js"
+  },
+  {
+    "revision": "68f5801b4b62c6344d8a",
+    "url": "/static/js/main.d4da7d47.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
