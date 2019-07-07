337e8a2fbbcb992cd0308919a67650f80d79943e
jianhuap <jianhuap@qq.com>
2019-07-07 14:44:25 +0800

    Updates

--- precache-manifest.053773d8a91a9c3b0cc19fd72689eb40.js
+++ precache-manifest.053773d8a91a9c3b0cc19fd72689eb40.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43557a8614adad5006fcd1199070e450",
+    "revision": "57894b93169f89b0179c05b688f56e90",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "21145a4eae09aaa11891",
+    "revision": "48a7312d1169a053ef18",
     "url": "/static/css/main.8ba60040.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.56b54301.chunk.js"
   },
   {
-    "revision": "21145a4eae09aaa11891",
-    "url": "/static/js/main.4efad8a7.chunk.js"
+    "revision": "48a7312d1169a053ef18",
+    "url": "/static/js/main.fa472508.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
